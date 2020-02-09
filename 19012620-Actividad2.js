3b82f399044f4450f59414a4b08b5838858af602
gabriela-O <gabriela.ortiz@galileo.edu>
2020-02-09 16:04:05 -0600

    initial commit

---
+++ 19012620-Actividad2.js
@@ -0,0 +1,13 @@
+var mes = 10;
+
+if(mes == 12 || mes == 1 || mes == 2){
+	document.write("es invierno");
+} else if(mes >= 3 && mes <= 5){
+	document.write("es primavera");
+} else if(mes >= 6 && mes <= 8){
+	document.write("es verano");
+} else if(mes >= 9 && mes <= 11){
+	document.write("es otoño");
+}
+
+document.write("<br>carné: 19012620<br>Nombre: Karla Gabriela Ortiz Cruz");
